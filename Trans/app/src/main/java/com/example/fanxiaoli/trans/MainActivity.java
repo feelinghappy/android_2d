d81882b8e75c90e37805445d5f988b7a803e72e6
fanxiaoli <312889426@qq.com>
2017-07-17 15:58:32 +0800

    转场动画

---
+++ Trans/app/src/main/java/com/example/fanxiaoli/trans/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.fanxiaoli.trans;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.transition.Transition;
+import android.util.Pair;
+import android.view.View;
+import android.view.animation.Transformation;
+
+public class MainActivity extends Activity {
+
+    private Intent intent;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_transition);
+    }
+    // 设置不同动画效果
+    public void explode(View view) {
+        intent = new Intent(this, Transitions.class);
+        intent.putExtra("flag", 0);
+        startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this)
+                        .toBundle());
+    }
+    // 设置不同动画效果
+    public void slide(View view) {
+        intent = new Intent(this, Transitions.class);
+        intent.putExtra("flag", 1);
+        startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this)
+                        .toBundle());
+    }
+    // 设置不同动画效果
+    public void fade(View view) {
+        intent = new Intent(this, Transitions.class);
+        intent.putExtra("flag", 2);
+        startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(this)
+                        .toBundle());
+    }
+    // 设置不同动画效果
+    public void share(View view) {
+        View fab = findViewById(R.id.fab_button);
+        intent = new Intent(this, Transitions.class);
+        intent.putExtra("flag", 3);
+        // 创建单个共享元素
+//        startActivity(intent,
+//                ActivityOptions.makeSceneTransitionAnimation(
+//                        this, view, "share").toBundle());
+        startActivity(intent,
+                ActivityOptions.makeSceneTransitionAnimation(
+                        this,
+                        // 创建多个共享元素
+                        Pair.create(view, "share"),
+                        Pair.create(fab, "fab")).toBundle());
+    }
+}
+
+
